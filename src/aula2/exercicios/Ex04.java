c060f7a48f76023e17aad546bbc253e9387be03f
Mariana <marianacavalle1@gmail.com>
2023-03-08 22:14:16 -0300

    Exs

---
+++ src/aula2/exercicios/Ex04.java
@@ -0,0 +1,13 @@
+package aula2.exercicios;
+import java.util.Scanner;
+
+public class Ex04 {
+    public static void main(String[]args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite o valor da compra: ");
+        String identificador = sc.nextLine();
+        
+    }
+}
